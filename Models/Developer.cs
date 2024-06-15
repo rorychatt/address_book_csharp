9fb41e8723876ba92c7ac98604720cb8978652dc
rorychatt <pavel.rinne@gmail.com>
2024-06-15 14:50:57 +0200

    test

---
+++ Models/Developer.cs
@@ -0,0 +1,8 @@
+namespace webapiDotNetTrainingGround.Models;
+
+public class Developer
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+}
