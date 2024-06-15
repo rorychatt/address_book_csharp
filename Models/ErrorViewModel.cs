7a921b6712330cc71f4639f67ccae78d688a8c7b
rorychatt <pavel.rinne@gmail.com>
2024-06-15 14:07:42 +0200

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace address_book_csharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
